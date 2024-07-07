754e3497bd19180a9fa2cec928eba0e3bfc6d407
maddy12day <madhavdubey10@gmail.com>
2024-07-07 11:39:50 +0530

    register and login api tested

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,8 +1,10 @@
 //importing the routes controller 
-import { login, register } from "../controller/Auth";
+import { login, register } from "../controller/Auth.js";
 import { Router } from "express";
 
 const router = Router();
 
-router.post('/login',login)
-router.post('/register',register)
+router.post('/login', login);
+router.post('/register', register);
+
+export default router;
